73896d92832fd83fccf90e0d9f664e3f952546c6
rahul-mundakkottil <121002647+rahul-mundakkottil@users.noreply.github.com>
2023-02-25 13:39:05 +0530

    Add files via upload

---
+++ Webview.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+    View
+}from 'react-native'
+//import { WebView } from 'react-native-webview';
+
+
+
+export default class Webview extends Component{
+    render(){
+        return <WebView source={{ uri: 'https://reactnative.dev/' }} />;
+    }
+}
